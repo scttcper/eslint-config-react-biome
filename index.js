d77c0bb83519feba59d29bf304fdb34b40b039b9
Scott Cooper <scttcper@gmail.com>
2024-01-27 13:38:48 -0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['xo-react/space', '@ctrl/eslint-config-biome'],
+  rules: {
+    'react/jsx-indent': 'off',
+    'react/jsx-indent-props': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prefer-read-only-props': 'off',
+  },
+};
